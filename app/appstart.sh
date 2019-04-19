d6f2492991281e1250bcd1e4221922bbfe9803a5
wolfharan <hari.rocker1299@gmail.com>
2019-04-19 15:30:24 +0000

    Changed name of docker image to acts:latest

--- app/appstart.sh
+++ app/appstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t acts-app .
-docker run -d  -p 80:80 --name acts1 --link db acts-app
+docker build -t acts:latest .
+docker run -d  -p 80:80 --name acts1 --link db acts:latest
